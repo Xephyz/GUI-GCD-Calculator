422f5dd4c222d29e6f5b183bf7644a6666600e89
Ahmad Sattar <thehabbos007@gmail.com>
2018-10-25 20:05:53 +0200

    Start mem

---
+++ src/com/GCDCalcGUI/Start.java
@@ -0,0 +1,14 @@
+package com.GCDCalcGUI;
+
+import javax.swing.*;
+
+public class Start {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GCD Calculator");
+        frame.setContentPane(new Frontend().getPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
