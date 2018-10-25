be5d1bffdfb7abdca36bd83db82d3c700c4e42c2
Anders Michaelsen <37267649+Xephyz@users.noreply.github.com>
2018-10-25 19:11:13 +0200

    Created GUI files
    Brought over the GUI files i was testing in another repo to this one dedicated to GCD Calculation :ok_hand:

---
+++ src/com/GCDCalcGUI/CalcGUIAlpha.java
@@ -0,0 +1,97 @@
+package com.GUI2;
+
+import javax.swing.*;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.DocumentFilter;
+import javax.swing.text.PlainDocument;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalcGUIAlpha {
+    private JButton button1;
+    private JPanel panel1;
+    private JTextField a6TextField;
+    private JTextField a24TextField;
+
+    public CalcGUIAlpha() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Hi!");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GCD Calculator");
+        frame.setContentPane(new CalcGUIAlpha().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
+
+class MyIntFilter extends DocumentFilter {
+    @Override
+    public void insertString(FilterBypass fb, int offset, String string,
+                             AttributeSet attr) throws BadLocationException {
+
+        Document doc = fb.getDocument();
+        StringBuilder sb = new StringBuilder();
+        sb.append(doc.getText(0, doc.getLength()));
+        sb.insert(offset, string);
+
+        if (test(sb.toString())) {
+            super.insertString(fb, offset, string, attr);
+        } else {
+            // warn the user and don't allow the insert
+        }
+    }
+
+    private boolean test(String text) {
+        try {
+            Integer.parseInt(text);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public void replace(FilterBypass fb, int offset, int length, String text,
+                        AttributeSet attrs) throws BadLocationException {
+
+        Document doc = fb.getDocument();
+        StringBuilder sb = new StringBuilder();
+        sb.append(doc.getText(0, doc.getLength()));
+        sb.replace(offset, offset + length, text);
+
+        if (test(sb.toString())) {
+            super.replace(fb, offset, length, text, attrs);
+        } else {
+            // warn the user and don't allow the insert
+        }
+
+    }
+
+    @Override
+    public void remove(FilterBypass fb, int offset, int length)
+            throws BadLocationException {
+        Document doc = fb.getDocument();
+        StringBuilder sb = new StringBuilder();
+        sb.append(doc.getText(0, doc.getLength()));
+        sb.delete(offset, offset + length);
+
+        if (test(sb.toString())) {
+            super.remove(fb, offset, length);
+        } else {
+            // warn the user and don't allow the insert
+        }
+
+    }
+}
